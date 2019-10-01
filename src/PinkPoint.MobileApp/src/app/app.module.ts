31a889818f59b6aed9d05fc94148622a565263fe
Michael Lehmann <lehmamic@bluewin.ch>
2019-10-01 20:55:29 +0200

    chore(mobile-app): install ngrx dev tools

--- src/PinkPoint.MobileApp/src/app/app.module.ts
+++ src/PinkPoint.MobileApp/src/app/app.module.ts
@@ -12,6 +12,8 @@ import { StoreModule } from '@ngrx/store';
 import { reducers, metaReducers } from './reducers';
 import { EffectsModule } from '@ngrx/effects';
 import { AppEffects } from './app.effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [AppComponent],
@@ -22,7 +24,7 @@ import { AppEffects } from './app.effects';
         strictStateImmutability: true,
         strictActionImmutability: true
       }
-    }), EffectsModule.forRoot([AppEffects])],
+    }), EffectsModule.forRoot([AppEffects]), StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })],
   providers: [
     StatusBar,
     SplashScreen,
